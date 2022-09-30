3a1d37ddfea29ad15a35c7dedb1c4d6a51b7f1f9
banamork <banamork@mail.ru>
2022-09-30 11:06:06 +0300

    start

---
+++ bernulli.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class lemniscata_bernulli {
+    public:
+
+
+
+    private:
+    int x;
+    int y;
+
+};
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+#include "bernulli.h"
+
+using namespace std;
+
+int main(){
+
+}
