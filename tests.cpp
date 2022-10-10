8c34bd86083959afc62532ca78a15c6139a52ca9
Banamork <banamork@mail.ru>
2022-10-10 16:47:37 +0300

    tests

--- tests.cpp
+++ tests.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include "bernulli.hpp"
+#include <cmath>
 
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
 
 TEST_CASE( "Factorials are computed", "[factorial]" ) {
+    std::cout << "Тест запущен." << std::endl;
     lemniscata_bernulli test;
-    REQUIRE( test.return_distance(60) == 12.7607 );
-    REQUIRE( test.radius(60) == 850.715 );
-    REQUIRE( test.sector_area(60) == 86.1162 );
+    REQUIRE( round(test.return_distance(60)*10000)/10000 == 12.7607 );
+    REQUIRE( round(test.radius(60)*1000)/1000 == 850.715 );
+    REQUIRE( round(test.sector_area(60)*10000)/10000 == 86.1162 );
     REQUIRE( test.square() == 50 );
 }
